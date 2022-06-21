f83b686d8913817d0a0df072599b382d51260710
Adam Pedracki <a.pedracki@piwik.pro>
2022-06-21 08:28:49 +0200

    image sending

--- cmd/main.go
+++ cmd/main.go
@@ -7,27 +7,35 @@ import (
 
 	"github.com/Protezeusz/tictactoe/pkg/files"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	file := files.NewIOFile("board.png")
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
+
 	r.POST("/", func(c *gin.Context) {
 		requestBody, err := ioutil.ReadAll(c.Request.Body)
 		if err != nil {
-			log.Fatalf("failed read: %s", err)
+			log.Fatalf("failed: %s", err)
 			c.String(http.StatusBadRequest, "Invalid body.")
 		}
 
-		errIMg := files.SaveRequestImage(requestBody)
+		errIMg := file.SaveRequestImage(requestBody)
 		if errIMg != nil {
 			log.Fatalf("failed save: %s", err)
-			c.String(http.StatusBadRequest, "Invalid body.")
+			c.String(http.StatusInternalServerError, "Internal server error.")
 		}
 
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		// play game
+
+		// response
+		c.File("rectangle.png")
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Protezeusz/tictactoe
 
 go 1.16
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/gin-contrib/cors v1.3.1
+	github.com/gin-gonic/gin v1.8.1
+)
